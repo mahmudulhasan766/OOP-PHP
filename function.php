6114391a512eafd58807a6252ebfe5b13a5fd5c9
HaSaN <mahamudul951@gmail.com>
2020-12-29 02:27:19 +0600

    solve problem

--- function.php
+++ function.php
@@ -1,16 +1,16 @@
 <?php
 class Calculation{
     function add($a,$b){
-        echo "Ans =".($a+$b)."<br/>";
+        echo "Add Ans =".($a+$b)."<br/>";
     }
     function sub($a,$b){
-        echo "Ans =".($a-$b)."<br/>";
+        echo "Sub Ans =".($a-$b)."<br/>";
     }
     function mul($a,$b){
-        echo "Ans =".($a*$b)."<br/>";
+        echo "Mul Ans =".($a*$b)."<br/>";
     }
     function div($a,$b){
-        echo "Ans =".($a/$b)."<br/>";
+        echo "Div Ans =".($a/$b)."<br/>";
     }
 }
 
--- index.php
+++ index.php
@@ -2,36 +2,29 @@
 <!DOCTYPE html>
 <html>
 <body>
-<!-- <from action="" method = "POST">
+<!-- <from action="" method="post">
     <table>
         <tr>
             <td>Enter the first Number: </td>
-            <td><input type = "number" name = "num1"/></td>
+            <td><input require type = "number" name = "num1"/></td>
         </tr>
         <tr>
             <td>Enter the second Number: </td>
-            <td><input type = "number" name = "num2"/></td>
+            <td><input require type = "number" name = "num2"/></td>
         </tr>
         <tr>
             <td>Enter the second Number: </td>
-            <td><input type = "submit" name = "calculation" value="Calculate"/></td>
+            <td><input type = "submit" name = "calculat" value="Calculate"></td>
         </tr>
     </table>
 </from> -->
+
 <form action="" method="post">
     First Number : <input require type = "number" name = "num1"/><br/><br/>
     Second Number : <input require type = "number" name = "num2"/><br/><br/>
     
-    Dristic: 
-    <select name="dristic" require>
-        <option value = "">Select Dristic</option>
-        <option value = "jamalpur">Jamalpur</option>
-        <option value = "dhaka">Dhaka</option>
-        <option value = "rongpur">Rongpur</option>
-        <option value = "tangail">Tangail</option>
-    </select>
-    <br><br>
-    <input type = "submit" value = "Submit">
+
+    <input type = "submit" name="calculat" value = "Submit">
 
 
 </form>
@@ -42,7 +35,9 @@
 
 <?php 
 include "function.php";
-if ($_SERVER['REQUEST_METHOD']=='POST'){
+//if ($_SERVER['REQUEST_METHOD']=='POST')
+if (isset($_POST['calculat']))
+{
     //$cal = $_POST['calculation'];
     $numOne = $_POST['num1'];
     $numTwo = $_POST['num2'];
