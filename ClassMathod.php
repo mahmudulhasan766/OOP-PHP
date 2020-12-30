acdda98ef99044e9008bc3bdd2a779903a91ad6c
HaSaN <mahamudul951@gmail.com>
2020-12-30 20:24:43 +0600

    Class and Mathod

--- ClassMathod.php
+++ ClassMathod.php
@@ -4,7 +4,13 @@ include "Student.php";
 
 if (class_exists("Students")){
     $student = new Students();
-    $student->describe();
+   // $student->describe();
+    if (method_exists($student, "describeu")){
+        $student->describe();
+    }else{
+        echo "Mathod is not found";
+    }
+
 }else {
     echo "Class not found";
 }
